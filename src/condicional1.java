a2f05c5c1273b2f44bde257c9e71d8bf8e2be16e
Heran76 <heran76@gmail.com>
2026-05-28 21:01:55 +0200

    Condicionales

---
+++ src/condicional1.java
@@ -0,0 +1,8 @@
+public class condicional1 {
+    public static void main(String[] args){
+        int a = 3;
+        if(a >= 3){
+            System.out.println("El número es mayor o igual a 3");
+        }
+    }
+}
---
+++ src/condicional2.java
@@ -0,0 +1,10 @@
+public class condicional2 {
+    public static void main(String[] args){
+        int a = 45;
+        if(a <= 42){
+            System.out.println("El numeros es menor o igual a 42");
+        }else{
+            System.out.print("El numero es mayor a 42");
+        }
+    }
+}
