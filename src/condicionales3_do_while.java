24e4345d66877acd9da14371c4c42b0dbd41b7f7
Heran76 <heran76@gmail.com>
2026-06-01 20:45:35 +0200

    condicionales3_do_while

---
+++ src/condicionales3_do_while.java
@@ -0,0 +1,9 @@
+public class condicionales3_do_while {
+    public static void main(String[] args){
+        int i = 0;
+        do{
+            System.out.println("Iteracción: " + i);
+            i++;
+        }while (i < 42);
+    }
+}
