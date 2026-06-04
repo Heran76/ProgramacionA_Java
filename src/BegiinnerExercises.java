5c6b62eddebe44135b229691926d34e64d5ac633
Heran76 <heran76@gmail.com>
2026-06-04 18:46:41 +0200

    BeginnerExercise

---
+++ src/BegiinnerExercises.java
@@ -0,0 +1,32 @@
+public class BegiinnerExercises {
+    public static void main(String[] args){
+        // Exercise 1 .Declara una variable de tipo String y asignale tu nombre.
+        String nombre = "Antonio";
+        System.out.println("Mi nombre es " + nombre);
+        // Exercise 2 . Crea una variable de tipo int y asignale tu edad.
+        int edad = 50;
+        System.out.println("Mi edad es de " + edad + " años");
+        // Exercise 3. Crea una variable double con tu alturaen metros-
+        double altura = 1.85;
+        System.out.println("Mi altura es de " + altura);
+        // Exercise 4. Declara una variable de top boolean que indiqque si te gusta programar
+        boolean  programar = true;
+        System.out.println("Me gusta programar " + programar);
+        // Exercise 5. Declara una constante con tu email.
+        final String email = "heran76@gmail.com";
+                System.out.println("Mi correo es "+ email);
+        //Exercise 6. Crea una variable de tipo char que guarde tu incial
+        char letra = 'A';
+        System.out.println("Mi letra es " + letra);
+
+        //exercise 7. Declara una variable de tipo sting con tu localidad y a continuación cambiala
+        //cambia su valor y vuelve a imprmir
+        String mytown = "Huesa";
+        System.out.println("Mi pueblo es " + mytown);
+        mytown = "Burela";
+        System.out.println("Mi pueblo es " + mytown);
+
+
+
+    }
+}
