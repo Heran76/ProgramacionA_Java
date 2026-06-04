50dd4780055703cac8e87ebc3786de5ad493a7da
Heran76 <heran76@gmail.com>
2026-06-04 21:21:25 +0200

    Funciones sin parametros sin retorno

---
+++ src/funciones_sin_parametros_sin_retorno.java
@@ -0,0 +1,8 @@
+public class funciones_sin_parametros_sin_retorno {
+    public static void main(String[] args){
+        imprimirMensaje();
+    }
+    public static void imprimirMensaje(){
+        System.out.println("!Bienvenido al mundo de Java¡");
+    }
+}
